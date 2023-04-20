9c3ab7c77bf59d72fd1d6095f75b9a908bd89ab1
EmilceAide <emilceaide.t@gmail.com>
2023-04-19 20:59:48 -0500

    models

--- src/db.js
+++ src/db.js
@@ -1,10 +1,18 @@
 const { Sequelize } = require('sequelize');
+const userModel = require('./models/User');
+const productModel = require('./models/Product');
+const  orderModel = require('./models/Order');
+
 require('dotenv').config()
 const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
 
 const database = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false
-})
+});
+
+userModel(database);
+productModel(database);
+orderModel(database);
 
 module.exports ={
     database, 
