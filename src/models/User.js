8ae5a439bff711c713f74cd6ba44d8d2d5c75fa6
EmilceAide <emilceaide.t@gmail.com>
2023-04-19 20:31:01 -0500

    user model

---
+++ src/models/User.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (database) => {
+    database.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true, 
+            autoIncrement: true
+        }, 
+        email: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        }
+    })
+}
